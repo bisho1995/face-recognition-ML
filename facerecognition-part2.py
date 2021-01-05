d168f53ee0c39a8b9fabd1d60f3fb3cab2dd2c07
Bisvarup Mukherjee <bisvarup.m@flipkart.com>
2021-01-05 09:39:25 +0530

    feat: added gitignore

--- facerecognition-part2.py
+++ facerecognition-part2.py
@@ -7,6 +7,19 @@ from os.path import isfile, join
 
 # Get the training data we previously made
 data_path = 'face/'
+data_paths = {
+    "ananya": "face/ananya",
+    "komal": "face/komal",
+}
+only_files = {
+    "ananya": [],
+    "komal": []
+}
+for key in data_paths:
+    only_files[key] = [f for f in listdir(data_paths[key]) if isfile(join(data_paths[key], f))]
+
+
+
 onlyfiles = [f for f in listdir(data_path) if isfile(join(data_path, f))]
 
 # Create arrays for training data and labels
@@ -33,3 +46,69 @@ print("Model trained sucessefully")
 # Let's train our model
 model.train(np.asarray( Training_Data), np.asarray(Labels))
 print("Model trained sucessefully")
+
+
+
+
+
+
+# Import the modules
+import cv2
+import numpy as np
+import time
+
+# Load HAAR face classifier
+face_classifier = cv2.CascadeClassifier( 'haarcascade_frontalface_default.xml')
+
+# Function to detect face
+def face_detector(img, size=0.5):
+    # Convert image to grayscale
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    faces = face_classifier.detectMultiScale( gray, 1.3, 5)
+    # If face not found return blank region
+    if faces is ():
+        return img, []
+    # Obtain Region of face
+    for (x,y,w,h) in faces:
+        cv2.rectangle(img, (x,y), (x+w,y+h), (0,255,255),2)
+        roi = img[y:y+h, x:x+w]
+        roi = cv2.resize(roi, (200, 200))
+    return img, roi
+
+
+# Open Webcam
+cap = cv2.VideoCapture(0)
+while True:
+    ret, frame = cap.read()
+    image, face = face_detector(frame)
+    try:
+        face = cv2.cvtColor(face, cv2.COLOR_BGR2GRAY)
+        # Pass face to prediction model
+        # "results" comprises of a tuple containing the label and the confidence value
+        results = model.predict(face)
+        # Tell about the confidence of user.
+        if results[1] < 500:
+            confidence = int( 100 * (1 - (results[1])/400) )
+            display_string = str(confidence) + '% Confident it is User'
+        cv2.putText(image, display_string, (100, 120), cv2.FONT_HERSHEY_COMPLEX, 1, (255,120,150), 2)
+        # If confidence is greater than 90 then the face will be recognized.
+        if confidence > 80:
+            cv2.putText(image, "Unlocked", (250, 450), cv2.FONT_HERSHEY_COMPLEX, 1, (0,255,0), 2)
+            cv2.imshow('Face Recognition', image )
+        # If confidence is less than 90 then the face will not be recognized.
+        else:
+            cv2.putText(image, "Locked", (250, 450), cv2.FONT_HERSHEY_COMPLEX, 1,  (0,0,255), 2)
+            cv2.imshow('Face Recognition', image )
+    # Raise exception in case, no image is found
+    except:
+        cv2.putText(image, "No Face Found", (220, 120) , cv2.FONT_HERSHEY_COMPLEX, 1, (0,0,255), 2)
+        cv2.putText(image, "Locked", (250, 450), cv2.FONT_HERSHEY_COMPLEX, 1, (0,0,255), 2)
+        cv2.imshow('Face Recognition', image )
+        pass
+    # Breaks loop when enter is pressed
+    if cv2.waitKey(1) == 13: #13 is the Enter Key
+        break
+
+# Release and destroyAllWindows
+cap.release()
+cv2.destroyAllWindows()
